6fcd6ebdde575359c380dd0b34997017b31ce3ea
karel.duransocarras0@gmail.com <karel.duransocarras0@gmail.com>
2025-05-03 23:49:43 -0400

    Agregar archivos de proyecto.

---
+++ SendMail.Client/SendMail.cs
@@ -0,0 +1,12 @@
+﻿using SendMail.Logic.Implementation;
+
+namespace SendMail.Client
+{
+    internal class SendMail
+    {
+        static void Main(string[] args)
+        {
+            SendService.Send("tucorreo@gmail.com", "tuclave", "destino@gmail.com", "asunto del correo", "contenido del correo");
+        }
+    }
+}
---
+++ SendMail.Logic/Implementation/SendService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SendMail.Logic.Implementation
+{
+    public static class SendService
+    {
+        /// <summary>
+        /// Method Sending Mail
+        /// </summary>
+        /// <param name="origin">Email address from where it will be sent.</param>
+        /// <param name="password">Email password from where it was sent.</param>
+        /// <param name="destiny">Destination email address.</param>
+        /// <param name="subject">Subject of the email.</param>
+        /// <param name="body">Contents of the email.</param>
+        /// <returns>True if sent</returns>
+        public static bool Send(string origin, string password, string destiny, string subject, string body)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+
+                client.Credentials = new NetworkCredential(origin, password);
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(origin);
+                mail.To.Add(destiny);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+                client.Send(mail);
+
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
